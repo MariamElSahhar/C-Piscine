8d63638ed85695df226e907555eb3ef3c8ba7c5e
MariamElSahhar <me1693@nyu.edu>
2023-03-23 16:07:21 +0400

    c00 files

---
+++ C00repo/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 18:52:04 by melsahha          #+#    #+#             */
+/*   Updated: 2022/09/18 19:11:48 by melsahha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter --;
+	}
+}
